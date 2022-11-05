a315cb25b36bbd1f904201785ae6da362510efa1
bsa333 <stanislav_ayratovich@mail.ru>
2022-11-05 06:31:11 +0500

    wooow

---
+++ Desktop/буткемп 13/videotest.py
@@ -0,0 +1,21 @@
+import cv2
+
+face_cascades = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_frontalface_default.xml")
+  
+cap = cv2.VideoCapture(0)
+
+while True:
+    __, frame = cap.read()
+    
+    img_gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    faces = face_cascades.detectMultiScale(img_gray)
+
+    for (x, y, w, h) in faces:
+        cv2.rectangle(frame, (x, y), (x + w, y + h), (0,0,255), 2)
+
+    cv2.imshow("Result", frame)
+
+    if cv2.waitKey(1) & 0xff == ord("q"):
+        break
+
